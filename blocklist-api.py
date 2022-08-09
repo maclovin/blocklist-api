c84c26162d46f7b2459a6bbe97ea8a1c5cb8dcf5
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2022-08-09 15:04:13 -0300

    Adding find ips method/route.

--- blocklist-api.py
+++ blocklist-api.py
@@ -10,28 +10,54 @@ cursor = connection.cursor()
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-def fetch_ips():
+def fetch_all_ips():
     ips = []
     connection.row_factory = sqlite3.Row
-    cursor.execute("SELECT * FROM firehol")
+    cursor.execute("SELECT * FROM firehol;")
     rows = cursor.fetchall()
     
     for i in rows:
-        print(i[0])
         ips.append(i[0])
 
     return ips
+
+def fetch_ips(ips_list):
+    found_ips = []
+    query = "SELECT * FROM firehol WHERE ip IN (%s);" %(", ".join([str("'" + ip + "'") for ip in ips_list]))
+    connection.row_factory = sqlite3.Row
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for i in rows:
+        found_ips.append(i[0])
+
+    sam_found_ips = list(set(found_ips))
+    
+    return sam_found_ips
 
 @app.route("/", methods=["GET"])
 def home():
     return "<h1>Blocklist API</h1><p>To be described...</p>"
 
-@app.route("/ips", methods=["GET"])
-def get_ips():
+@app.route("/list", methods=["GET"])
+def get_list():
     offset = int(request.args.get("offset"))
-    per_page = int(request.args.get("per_page"))
-    ips = fetch_ips()
+    per_page = int(request.args.get("perPage"))
+    ips = fetch_all_ips()
+
     return jsonify(ips[offset: offset + per_page])
+
+@app.route("/find", methods=["GET"])
+def find_ips():
+    ips = request.args.get("ips")
+    print(ips)
+    
+    split_ips = ips.split(",")
+    found_ips = fetch_ips(split_ips)
+    not_found_ips = list(set(split_ips).symmetric_difference(set(found_ips)))
+    response = {"found": found_ips, "notFound": not_found_ips}
+
+    return jsonify(response)
 
 app.run(debug=True)
 
