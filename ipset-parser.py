9295e39b2a9ae15703a7128001a76061daa14759
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2022-08-09 01:37:58 -0300

    Improving ipset parser and updating db.

--- ipset-parser.py
+++ ipset-parser.py
@@ -1,5 +1,7 @@
 import re
+import glob
 from db import connection, cursor
+from os import path
 
 def generate_query(ip_list):
     query = "DELETE * FROM firehol;"
@@ -10,18 +12,41 @@ def generate_query(ip_list):
     query = query[:-1] + ";"
     return query
 
-def parse_ips():
-    ipset_file = open("./ipsets/blocklist_de.ipset", "r")
-    ipset_text = ipset_file.read()
-    ipset_file.close()
-    pattern = re.compile(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})")
-    ipset_ips = pattern.findall(ipset_text)
+def parse_ips(path_to_file=""):
+    try:
+        ipset_file = open(path_to_file, "r")
+        ipset_text = ipset_file.read()
+        ipset_file.close()
+        pattern = re.compile(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})")
+        ipset_ips = pattern.findall(ipset_text)
+    except:
+        return []
+    
     return ipset_ips
 
-ips = parse_ips()
-ips_to_insert_query = generate_query(ips)
-cursor = cursor.execute(ips_to_insert_query)
-connection.commit()
-connection.close()
+def main():
+    ips = []
 
-print("%d IPs was added." %(len(ips)))
+    for ipset_file in glob.glob("./blocklist-ipsets/*.ipset"):
+        print("Parsing IPs from %s..." %(ipset_file))
+        parsed_ipset = parse_ips(ipset_file)
+        ips = ips + parsed_ipset
+    
+    print("Inserting IPs in local database...")
+    ips_to_insert_query = generate_query(ips)
+    cursor.execute(ips_to_insert_query)
+    connection.commit()
+    connection.close()
+
+    print("%d IPs was added secessfully!" %(len(ips)))
+
+if __name__ == "__main__":
+    main()
+
+ips = parse_ips()
+#ips_to_insert_query = generate_query(ips)
+#cursor = cursor.execute(ips_to_insert_query)
+#connection.commit()
+#connection.close()
+
+#print("%d IPs was added." %(len(ips)))
